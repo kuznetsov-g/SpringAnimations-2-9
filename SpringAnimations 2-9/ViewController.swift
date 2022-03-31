e744b3ee66568f4137dcc1c1ffa440eb71dbea9c
George <backs159@icloud.com>
2022-03-31 21:12:12 +0500

    add logic for views and arrays with sets for animations

--- SpringAnimations 2-9/ViewController.swift
+++ SpringAnimations 2-9/ViewController.swift
@@ -5,15 +5,53 @@
 //  Created by Георгий Кузнецов on 29.03.2022.
 //
 
-import UIKit
+import Spring
 
 class ViewController: UIViewController {
+    
+    @IBOutlet weak var movingView: SpringButton!
+    
+    @IBOutlet weak var animationLabel: UILabel!
+    @IBOutlet weak var curveLabel: UILabel!
+    @IBOutlet weak var forceLabel: UILabel!
+    @IBOutlet weak var repeatCountLabel: UILabel!
+    
+    var animation = ""
+    var curve = ""
+    var force = CGFloat(0)
+    var repeatCount = Float(0)
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        getLabelInfo()
     }
 
-
+    func getLabelInfo() {
+        animationLabel.text = "animation: \(animation )"
+        curveLabel.text = "curve: \(curve )"
+        forceLabel.text = "force: \(force)"
+        repeatCountLabel.text = "repeats: \(repeatCount)"
+        
+        getRandomSets()
+    }
+    
+    func getRandomSets() {
+        animation = animationArray.randomElement() ?? "shake"
+        curve = curveArray.randomElement() ?? "easeIn"
+        force = CGFloat(Int.random(in: 1...5))
+        repeatCount = Float(Int.random(in: 1...3))
+    }
+    
+    
+    @IBAction func goButton(_ sender: Any) {
+        getLabelInfo()
+        movingView.animation = animation
+        movingView.curve = curve
+        movingView.force = force
+        movingView.repeatCount = repeatCount
+        
+        movingView.animate()
+    }
 }
 
---
+++ SpringAnimations 2-9/animationData.swift
@@ -0,0 +1,47 @@
+//
+//  animationData.swift
+//  SpringAnimations 2-9
+//
+//  Created by Георгий Кузнецов on 31.03.2022.
+//
+
+import Foundation
+
+let animationArray =
+[
+  "shake"
+ ,"pop"
+ ,"morph"
+ ,"squeeze"
+ ,"wobble"
+ ,"swing"
+ ,"flipX"
+ ,"flipY"
+ ,"fall"
+ ,"squeezeLeft"
+ ,"squeezeRight"
+ ,"squeezeDown"
+ ,"squeezeUp"
+ ,"slideLeft"
+ ,"slideRight"
+ ,"slideDown"
+ ,"slideUp"
+ ,"fadeIn"
+ ,"fadeOut"
+ ,"fadeInLeft"
+ ,"fadeInRight"
+ ,"fadeInDown"
+ ,"fadeInUp"
+ ,"zoomIn"
+ ,"zoomOut"
+ ,"flash"
+]
+
+let curveArray =
+[
+  "spring"
+ ,"linear"
+ ,"easeIn"
+ ,"easeOut"
+ ,"easeInOut"
+]
